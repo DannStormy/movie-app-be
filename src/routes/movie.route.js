9cbf1b1c0c78722326d4473c9d1149d0424c454a
DannStormy <dillydot22@gmail.com>
2022-11-24 14:57:38 +0100

    search by name, genre, year

--- src/routes/movie.route.js
+++ src/routes/movie.route.js
@@ -1,24 +1,15 @@
 import { Router } from "express";
 import {
   fetchMovies,
-  fetchMoviesPagination,
-  fetchMovieTitle,
-  fetchMovieRating,
-  fetchMovieGenre,
-  fetchMovieYear
+  searchMovies,
 } from "../controllers/movie";
 
 const router = Router();
 
 router.get("/", fetchMovies);
-// router.get("/", fetchMoviesPagination);
-// router.get("/title/", fetchMovieTitle);
-// router.get("/rating/:rating", fetchMovieRating);
-// router.get("/genre/", fetchMovieGenre);
-// router.get("/year/:year", fetchMovieYear);
+router.get("/search", searchMovies);  // get search by title, genre, year etc
 
 // router.get("/", fetchMovies);  // get all
-// router.get("/:movieId", fetchMovies);  // get one
 // router.get("/search", fetchMovies);  // get search by title, genre, year etc
 
 
