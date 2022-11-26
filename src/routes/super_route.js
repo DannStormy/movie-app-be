ccab012c0f94ae8a76bb6e323bbdf4f7646f62e8
DannStormy <dillydot22@gmail.com>
2022-11-26 21:48:00 +0100

    super create-admin, add movie

--- src/routes/super_route.js
+++ src/routes/super_route.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { superLogin, fetchUsers } from "../controllers/super.controller.js";
+import {
+  superLogin,
+  fetchUsers,
+  createNewAdmin,
+  addNewMovie,
+} from "../controllers/super.controller.js";
 import AccessControlMiddleware from "../middlewares/accessControl";
 import SuperMiddleware from "../middlewares/super.middleware";
 import UserMiddleware from "../middlewares/user.js";
@@ -9,11 +14,24 @@ import schema from "../validations/schema.js";
 const router = Router();
 
 const { isSuper } = AccessControlMiddleware;
-const { loginSchema } = schema;
-const { checkDetails } = SuperMiddleware;
+const { loginSchema, createAdminSchema, addMovieSchema } = schema;
+const { checkDetails, checkAdminExists } = SuperMiddleware;
 const { authenticate, validate } = UserMiddleware;
 
 router.get("/users", [authenticate, isSuper], fetchUsers);
 router.post("/login", validate(loginSchema), checkDetails, superLogin);
+router.post(
+  "/create-admin",
+  validate(createAdminSchema),
+  checkAdminExists,
+  [authenticate, isSuper],
+  createNewAdmin
+);
+router.post(
+  "/add-movie",
+  validate(addMovieSchema),
+  [authenticate, isSuper],
+  addNewMovie
+);
 
 export default router;
